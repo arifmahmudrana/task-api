65c3f3483e0a112f2875986e4fe4c40983235bea
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2019-04-16 23:30:15 +0600

    debugging heroku checking user

--- src/models/OAuth.js
+++ src/models/OAuth.js
@@ -86,8 +86,17 @@ module.exports.revokeToken = token =>
  * Get user.
  */
 
-module.exports.getUser = (username, password) =>
-  User.findByCredentials(username, password).then(user => {
+module.exports.getUser = (username, password) => {
+  console.log('==================username==================');
+  console.log(username);
+  console.log('==================username==================');
+  console.log('==================password==================');
+  console.log(password);
+  console.log('==================password==================');
+  return User.findByCredentials(username, password).then(user => {
+    console.log('==================user==================');
+    console.log(user);
+    console.log('==================user==================');
     if (!user) {
       return false;
     }
@@ -96,6 +105,7 @@ module.exports.getUser = (username, password) =>
       id: user._id.toString()
     };
   });
+};
 
 /**
  * Save token.
