dfdd157341bb72a684d7665fd4cff3da8f96654f
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2019-04-18 00:11:42 +0600

    add user fetch & set to request middleware

---
+++ src/middlewares/user.js
@@ -0,0 +1,21 @@
+const customErr = require('../utils/err');
+const { User } = require('../models/User');
+
+const findById = async (req, res, next) => {
+  try {
+    const user = await User.findById(res.locals.oauth.token.user.id);
+
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      throw customErr('Not Found', 404);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  findById
+};
