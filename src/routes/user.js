4055c99109ae32fd225c31810fd5af4fd5184e62
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2019-04-18 00:12:25 +0600

    restructure user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { User, userTransformer } = require('../models/User');
-const customErr = require('../utils/err');
+const { userTransformer } = require('../models/User');
 
-router.get('/me', async (req, res, next) => {
-  try {
-    const user = await User.findById(res.locals.oauth.token.user.id);
-
-    if (user) {
-      res.json(userTransformer(user));
-    } else {
-      throw customErr('Not Found', 404);
-    }
-  } catch (error) {
-    next(error);
-  }
+router.get('/', (req, res) => {
+  res.json(userTransformer(req.user));
 });
 
 module.exports = router;
