f033889f348b318da47eab634808bcb8159b8589
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2019-04-21 13:44:29 +0600

    task model

---
+++ src/models/Task.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const fields = {
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+    trim: true
+  },
+  description: {
+    type: String,
+    required: [true, 'Description is required'],
+    trim: true
+  },
+  done: {
+    type: Boolean,
+    default: false
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  }
+};
+const taskSchema = new Schema(fields, {
+  timestamps: true
+});
+taskSchema.index({ title: 'text' });
+taskSchema.index({ done: 1 });
+taskSchema.index({ createdAt: -1 });
+
+const taskTransformer = task => ({
+  id: task._id,
+  title: task.title,
+  description: task.description,
+  done: task.done
+});
+
+const Task = mongoose.modelNames().includes('Task')
+  ? mongoose.connection.model('Task')
+  : mongoose.model('Task', taskSchema);
+
+Task.on('index', err => {
+  if (err) {
+    console.error('Task index error: %s', err);
+  } else {
+    console.info('Task indexing complete');
+  }
+});
+
+module.exports = {
+  Task,
+  fields,
+  taskTransformer
+};
